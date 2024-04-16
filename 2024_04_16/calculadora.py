018e2be5e1e930df1f281b94df00ca7322aee1c6
LucasCosta <156377381+Luda-costa@users.noreply.github.com>
2024-04-16 11:16:01 -0300

    2024_04_16/calculadora.py

--- 2024_04_16/calculadora.py
+++ 2024_04_16/calculadora.py
@@ -1,6 +1,6 @@
-l=float(input("escreva a largura: "))#entrada
-a=float(input("escreva a altura: "))#entrada
-area=l*a #calculo area
-perimetro=2*(l+a) #calculo perimetro
-hipotenusa=(a**2+l**2)**(1/2) #calculo hipotenusa
+l = float(input("escreva a largura: "))#entrada
+a = float(input("escreva a altura: "))#entrada
+area = l * a #calculo area
+perimetro = 2 * (l+a) #calculo perimetro
+hipotenusa = (a ** 2 + l ** 2) ** (1/2) #calculo hipotenusa
 print("Area = ", area,"\nPerimetro =  ", perimetro, "\nHipotenusa = ", hipotenusa)#saida
